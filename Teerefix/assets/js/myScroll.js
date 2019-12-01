3ae88d4055167b3e321b01d244e6f7ff2476702a
Adamu Fura Suleiman <33485255+adamufura@users.noreply.github.com>
2019-12-02 00:18:00 +0100

    add files via upload

---
+++ Teerefix/assets/js/myScroll.js
@@ -0,0 +1,42 @@
+window.sr = ScrollReveal();
+sr.reveal(".nav", {
+    duration: 3000,
+    origin: "top",
+    distance: "300px"
+});
+sr.reveal(".container-one", {
+    duration: 2000,
+    origin: "left",
+    distance: "300px"
+});
+sr.reveal(".img_card", {
+    duration: 2000,
+    origin: "bottom",
+    distance: "300px"
+});
+sr.reveal(".btn_contact", {
+    duration: 2000,
+    delay: 2000,
+    origin: "bottom",
+    distance: "300px"
+});
+sr.reveal(".img_one", {
+    duration: 2000,
+    origin: "left",
+    distance: "300px"
+});
+sr.reveal(".img_two", {
+    duration: 2000,
+    origin: "right",
+    distance: "300px"
+});
+sr.reveal(".img_three", {
+    duration: 2000,
+    origin: "bottom",
+    distance: "100px"
+});
+sr.reveal(".img_four", {
+    duration: 2000,
+    origin: "bottom",
+    distance: "100px"
+});
